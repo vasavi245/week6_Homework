f565d5f8ff956adea09d8432b57b5dda3e99367c
vasavi245 <58574509+vasavi245@users.noreply.github.com>
2020-03-20 16:24:44 +0800

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,196 @@
+$(document).ready(function() {
+  var apiKey = "51cf29ba1bfca5fc3108cefcddf70638";
+  //var cityArray = [];
+
+  $("#searchBtn").click(function() {
+    var cityInput = $(".cityText")
+      .val()
+      .trim();
+    //var cityToStore = cityInput;
+
+    // clear input box
+    $(".cityText").val("");
+    console.log(cityInput);
+    storeInLocalStorage(cityInput);
+    displayLocalStorage();
+
+    function storeInLocalStorage() {
+      var cityToStore = cityInput;
+      for(i=0; i<cityInput.length; i++){
+      var strCities = window.localStorage.getItem("strCities") || "[]";
+      console.log(strCities);
+     
+      const cities = JSON.parse(strCities);
+      cities.push(cityToStore);
+      strCities = JSON.stringify(cities);
+      localStorage.setItem("Searched cities", strCities);
+      console.log("Local Storage: ", strCities);
+      }
+      
+    }
+
+    storeInLocalStorage();
+    function displayLocalStorage() {
+
+      var history = "";
+      if (localStorage.getItem("history") !== "") {
+          var history = localStorage.getItem("history");
+      }
+
+
+      if (history !== "") {
+          $("#lastResults").html(
+              "<b>Last Results:</b>" +
+              "<ul data-role=\"listview\" data-inset=\"true\" >" +
+              "<li><a href=\"#test\"> " + history + " </a></li>" +
+              "</ul>"
+          );
+      }
+  };
+
+
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      cityInput +
+      "&APPID=" +
+      apiKey;
+    var currentDate = moment()
+      .subtract(10, "days")
+      .calendar();
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(queryURL);
+      var tempM = (response.main.temp - 273.15) * 1.8 + 32;
+      var tempF = Math.floor(tempM);
+
+      $("#city").text(response.name);
+      $("#currentDateAndTime").html(currentDate);
+      $("#currentTempurature").html(`${tempF}&#176;F`);
+      $("#currentHumidity").html(response.main.humidity + "%");
+      $("#currentWindSpeed").html(response.wind.speed);
+      $("#weather_image").attr(
+        "src",
+        "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+      );
+      $("#description_weather").html(response.weather[0].description);
+      var lon = response.coord.lon;
+      var lat = response.coord.lat;
+      // api key url for forescast
+      uvURL =
+        "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+        apiKey +
+        "&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+      $.ajax({
+        url: uvURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        var uvIndex = response.value;
+        var uvColor = "";
+        if (uvIndex < 3) {
+          uvColor = "green";
+        } else if (uvIndex < 6) {
+          uvColor = "yellow";
+        } else if (uvIndex < 8) {
+          uvColor = "orange";
+        } else if (uvIndex < 11) {
+          uvColor = "red";
+        } else {
+          uvColor = "violet";
+        }
+        $("#currentUVIndex").html(uvIndex);
+        $("#currentUVIndex").attr("style", "background-color: " + uvColor);
+
+        makeList();
+        // fiveDayForecast();
+      });
+
+      var forecastURL =
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        cityInput +
+        "&APPID=" +
+        apiKey;
+
+      function makeList() {
+        let listCities = $("<a href=#>")
+          .addClass("list-group-item")
+          .text(cityInput);
+        $(".list-group-item-action").append(listCities);
+
+        $(".list-group-item").click(function() {
+          event.preventDefault();
+
+          city = $(this)
+            .prev()
+            .val();
+          $(cityInput).push(city);
+
+          if (cityInput == "") {
+            return;
+          }
+        });
+      }
+      $.ajax({
+        url: forecastURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+
+        let day_number = 0;
+
+        //iterate through the 40 weather data sets
+
+        for (let i = 0; i < response.list.length; i++) {
+          //split function to isolate the time from the time/data aspect of weather data, and only select weather reports for 3pm
+
+          if (response.list[i].dt_txt.split(" ")[1] == "15:00:00") {
+            //if time of report is 3pm, populate text areas accordingly
+
+            let day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
+
+            let month = response.list[i].dt_txt.split("-")[1];
+
+            let year = response.list[i].dt_txt.split("-")[0];
+
+            $("#" + day_number + "date").text(month + "/" + day + "/" + year);
+
+            let temp = Math.round(
+              ((response.list[i].main.temp - 273.15) * 9) / 5 + 32
+            );
+
+            $("#" + day_number + "five_day_temp").text(
+              "Temp: " + temp + String.fromCharCode(176) + "F"
+            );
+
+            $("#" + day_number + "five_day_humidity").text(
+              "Humidity: " + response.list[i].main.humidity + "%"
+            );
+
+            $("#" + day_number + "five_day_icon").attr(
+              "src",
+              "http://openweathermap.org/img/w/" +
+                response.list[i].weather[0].icon +
+                ".png"
+            );
+
+            console.log(response.list[i].dt_txt.split("-"));
+
+            console.log(day_number);
+
+            console.log(response.list[i].main.temp);
+
+            day_number++;
+          }
+        }
+      });
+    });
+
+    //})//response close
+  });
+});
